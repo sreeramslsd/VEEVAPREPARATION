6c83bbccf4f1a7eb0e3b81615bc4334676f07217
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:26:17 +0530

    Add Practice - Delete From (Unrated)

---
+++ codechef/Unrated/GSQ14 - Practice - Delete From.cpp
@@ -0,0 +1,11 @@
+/* Write a query which does the following
+- Delete the row where the department is Client.
+- Output the entire table.
+*/
+
+
+delete from employee where department = 'Client';
+
+
+
+select * from employee;
