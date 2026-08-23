94bd696b124805f45a76186096d54bc79c94a392
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 16:04:50 +0530

    Add Project - Sort Results using Order by (Unrated)

---
+++ codechef/Unrated/GSQ36 - Project - Sort Results using Order by.cpp
@@ -0,0 +1,9 @@
+/* You want to ensure that your delivery boys have to climb the least floors.
+Write a query to sort the data in an ascending order. */
+
+
+
+
+
+
+select * from Orders order by apartment_floor asc;
