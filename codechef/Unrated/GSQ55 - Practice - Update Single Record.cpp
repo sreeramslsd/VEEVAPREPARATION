eeba56671e4e7f1ed3f4b9809863214ade191687
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:30:57 +0530

    Add Practice - Update Single Record (Unrated)

---
+++ codechef/Unrated/GSQ55 - Practice - Update Single Record.cpp
@@ -0,0 +1,11 @@
+/* Write a query which does the following
+- Update the age to 52 for the customer with customer id 3.
+- Output all the entires of the table. */
+
+
+
+update customer set age = 52 where id = 3;
+
+
+
+select * from customer;
