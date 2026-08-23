173cb88864a83d07b8a7bbfc75ce85f3330a1239
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:38:24 +0530

    Add Practice - Basic query with Where Clause (Unrated)

---
+++ codechef/Unrated/GSQ20 - Practice - Basic query with Where Clause.cpp
@@ -0,0 +1,7 @@
+/* Write a query that does the following
+- Where the origin of the flight is 'New York'
+- Output the passenger_name and gender */
+
+
+
+select passenger_name ,  gender from Flights where origin = 'New York' ;
