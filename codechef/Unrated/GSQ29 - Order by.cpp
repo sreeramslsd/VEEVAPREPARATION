e648cb73797d5b94260a63dd807b88473abea749
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:51:40 +0530

    Add Order by (Unrated)

---
+++ codechef/Unrated/GSQ29 - Order by.cpp
@@ -0,0 +1,10 @@
+/* Write a query based on the following conditions 
+Destination is 'Cairo'
+Entries are ordered by passenger_name in a descending order
+*/
+
+
+
+
+
+SELECT * FROM Flights where Destination = 'Cairo' order by passenger_name DESC;
