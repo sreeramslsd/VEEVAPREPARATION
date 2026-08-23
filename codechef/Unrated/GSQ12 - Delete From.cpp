60ee5324caaf5d2122adaa168faf4c0d4216b768
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:23:18 +0530

    Add Delete From (Unrated)

---
+++ codechef/Unrated/GSQ12 - Delete From.cpp
@@ -0,0 +1,13 @@
+/* Write a query which does the following
+- Delete all rows in the employee table whose Department is 'Hr'.
+- Output all the entires of the table. */
+
+
+
+
+
+delete from employee where department = 'Hr';
+
+
+
+select * from employee;
