661165a7a568097a859da43a6e585fdc9840af80
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:31:45 +0530

    Add Practice - Delete Single Record (Unrated)

---
+++ codechef/Unrated/GSQ56 - Practice - Delete Single Record.cpp
@@ -0,0 +1,12 @@
+/*Write a query which does the following
+- Delete the entire row of the customer with customer id 1.
+- Output the entire table once the above action is performed. */
+
+
+
+
+delete from customer where id=1;
+
+
+
+select * from customer;
