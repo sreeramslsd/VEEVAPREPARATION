412ea868b426550d02e4123dc742c6f6073e628f
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:22:21 +0530

    Add Practice Problem - Update table (Unrated)

---
+++ codechef/Unrated/GSQ11 - Practice Problem - Update table.cpp
@@ -0,0 +1,12 @@
+/* Write a query to do the following
+- Set hourly_pay to 150 for HR employees
+- Output the entire table
+*/
+
+
+
+UPDATE EMPLOYEE SET 'Hourly_Pay' = 150 where Department = 'Hr';
+
+
+
+select * from employee;
