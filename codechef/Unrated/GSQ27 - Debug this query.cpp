e5030587d75809fab68f2297c0e85b960405fe15
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-23 15:49:55 +0530

    Add Debug this query (Unrated)

---
+++ codechef/Unrated/GSQ27 - Debug this query.cpp
@@ -0,0 +1,6 @@
+/* Debug this query to meet the following conditons
+- Passenger gender - Female AND
+- Flight destination - Delhi
+*/
+
+select * from Flights where gender = 'Female' and destination = 'Delhi';
