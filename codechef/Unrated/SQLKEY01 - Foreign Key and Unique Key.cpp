59f2aeb8227fb686abe30aee37e767b40662f237
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-24 20:31:39 +0530

    Add Foreign Key and Unique Key (Unrated)

---
+++ codechef/Unrated/SQLKEY01 - Foreign Key and Unique Key.cpp
@@ -0,0 +1,36 @@
+-- Write the SQL query to create the tables given above (Customers & Orders) with the given constraints like primary key, unique key, foreign key (do not insert data into the table).
+
+
+
+create table customers (
+
+
+customer_id int primary key, 
+
+customer_name text,
+
+city text ,
+
+
+customer_email text unique
+
+
+);
+
+
+create table orders(
+
+order_id int primary key,
+
+customer_id int ,
+
+
+order_date text,
+
+amount real,
+
+foreign key(customer_id) references Customers(customer_id)
+
+
+
+);
