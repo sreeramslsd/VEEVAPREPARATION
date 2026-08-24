8f613d0a5ca7724365031ae38220ed2a1214ca9b
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-24 19:53:44 +0530

    Add Self Joins (Unrated)

---
+++ codechef/Unrated/ASQL01C - Self Joins.cpp
@@ -0,0 +1,23 @@
+-- Part 1: Find pairs of students that belong to the same department
+SELECT 
+    s1.St_id,
+    s1.St_Name,
+    s1.Department,
+    s2.St_id,
+    s2.St_Name,
+    s2.Department
+FROM student AS s1
+INNER JOIN student AS s2
+ON s1.Department = s2.Department
+AND s1.St_id != s2.St_id;
+
+-- Part 2: Identify students who have chosen the same Course_id as their favorite
+SELECT DISTINCT
+    s1.St_id,
+    s1.St_Name,
+    s1.Course_id
+FROM student AS s1
+INNER JOIN student AS s2
+ON s1.Course_id = s2.Course_id
+AND s1.St_id != s2.St_id
+ORDER BY s1.Course_id;
