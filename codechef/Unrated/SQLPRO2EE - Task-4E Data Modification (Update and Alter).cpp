9d687cfd9dc8ddf9543607454016a4b292476a10
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-24 19:37:45 +0530

    Add Task-4E Data Modification (Update and Alter) (Unrated)

---
+++ codechef/Unrated/SQLPRO2EE - Task-4E Data Modification (Update and Alter).cpp
@@ -0,0 +1,11 @@
+/* Update your query here */
+
+
+
+
+update courses set department = 'Technology' where course_name = 'Computer Science';
+
+
+
+
+select course_id, course_name, department from courses where department='Technology'limit 1;
