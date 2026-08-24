17e1099de8459af65e6d4a870865522eaa90e8b6
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-24 19:45:35 +0530

    Add Combining tables with SQL (Unrated)

---
+++ codechef/Unrated/GSQ61A - Combining tables with SQL.cpp
@@ -0,0 +1,6 @@
+/* Write a query to join the table 'student' and 'course' using 'Course_id' to match both the tables and output the joined table. */
+
+
+
+
+SELECT * FROM STUDENT S1 JOIN COURSE C1 ON S1.COURSE_ID=C1.COURSE_ID;
