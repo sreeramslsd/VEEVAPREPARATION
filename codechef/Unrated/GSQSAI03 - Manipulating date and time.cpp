13d767549b1907551525dc52e64db11e438e6765
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-24 12:25:01 +0530

    Add Manipulating date and time (Unrated)

---
+++ codechef/Unrated/GSQSAI03 - Manipulating date and time.cpp
@@ -0,0 +1,10 @@
+/* Write a query which outputs the date of 1 month from '2024-07-04'.
+   Write a query which outputs the time of 30 minutes from '20:34:21'.*/
+
+
+
+
+select date('2024-07-04' , '+1 month');
+
+
+select time('20:34:21', '+30 minutes');
