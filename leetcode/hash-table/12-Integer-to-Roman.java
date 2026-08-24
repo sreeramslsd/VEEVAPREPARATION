9247885c094cb7c495e15a310a62792862e3006d
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-24 21:05:16 +0530

    Add Integer-to-Roman (hash-table)

--- leetcode/hash-table/12-Integer-to-Roman.java
+++ leetcode/hash-table/12-Integer-to-Roman.java
@@ -3,13 +3,28 @@ class Solution {
         int values[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         String symbols[]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         String roman="";
-        for(int i=0;i<values.length;i++){
-            if(num==0) break;
-            while(num>=values[i]){
-                roman+=symbols[i];
-                num-=values[i];
+
+        int n = values.length;
+
+
+
+        for(int i=0; i< n ; i++)
+        {
+            if(num==0)
+            break;
+
+            while(num >= values[i])
+
+
+
+            {
+                roman= roman + symbols[i];
+                num =  num - values[i]  ;
             }
         }
+
+
+
         return roman;   
     }
 }
