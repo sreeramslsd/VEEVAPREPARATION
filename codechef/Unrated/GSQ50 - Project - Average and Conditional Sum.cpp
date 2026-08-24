bcb22a66e698a9c9611c96e2e58cfb19c005f154
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-24 15:02:38 +0530

    Add Project - Average and Conditional Sum (Unrated)

---
+++ codechef/Unrated/GSQ50 - Project - Average and Conditional Sum.cpp
@@ -0,0 +1,9 @@
+/* Write a query to output the following on separate lines
+- The Average age of the students of the school
+- Total Outstanding Fee of the students in 'Arts' department */
+
+
+select avg(age) as avg_age from student ;
+
+
+select sum(fee_outstanding) as fee_outstanding from student where department = 'Arts';
