24d953a9d9d682260d186b425d3eaacce563423c
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-25 11:27:37 +0530

    Add Project - Inner Join Two Tables (Unrated)

---
+++ codechef/Unrated/GSQ68A - Project - Inner Join Two Tables.cpp
@@ -0,0 +1,8 @@
+/* Write a query to join the table 'Customer' and 'Purchase' using Customer_id as the common column in the table.
+Output the joined table. */
+
+
+
+
+
+select * from customer c join purchase p on c.Customer_id= p.Customer_id;
