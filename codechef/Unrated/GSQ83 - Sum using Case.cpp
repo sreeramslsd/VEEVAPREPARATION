0545b9f8fdde42afacb5a0500bbd011eb3205279
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-26 11:10:27 +0530

    Add Sum using Case (Unrated)

--- codechef/Unrated/GSQ83 - Sum using Case.cpp
+++ codechef/Unrated/GSQ83 - Sum using Case.cpp
@@ -5,4 +5,8 @@ Alias the sum column as 'Sum_Senior_Fee'.*/
 
 
 
-select department,  sum(fee) as Sum_Senior_Fee from student where age > 20 group by department;
+--select department,  sum(fee) as Sum_Senior_Fee from student where age > 20 group by department;
+
+
+
+select department , sum(case when age > 20 then fee else 0 end ) as sum_senior_fee from student group by department;
