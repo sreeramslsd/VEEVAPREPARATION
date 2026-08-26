d5e41041e7de999063f8b3f29c6698f928487469
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-26 11:36:57 +0530

    Add Project - Conditional SUM (Unrated)

---
+++ codechef/Unrated/GSQ85C - Project - Conditional SUM.cpp
@@ -0,0 +1,10 @@
+
+
+
+
+--select cuisine , sum (price ) as 'Non_Veg_Price' from Orders where category= 'Non veg' group by Cuisine;
+
+
+
+
+select cuisine , sum(case when category = 'Non veg' then price else 0 END) as 'Non_Veg_Price' from Orders group by Cuisine; 
