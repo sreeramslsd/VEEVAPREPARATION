f53392e5a8651d3101e704f0b98a44f9d1b8b0af
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 11:11:34 +0530

    Add Task-8 Patients Treated in Multiple Departments (Unrated)

---
+++ codechef/Unrated/ASQLPR109 - Task-8 Patients Treated in Multiple Departments.cpp
@@ -0,0 +1,19 @@
+-- Complete the query to find patients treated in multiple departments (specializations)
+-- (Use the concept of JOINS, Subqueries & Set Operations)
+
+-- The output should contain the following fields: 
+-- |PatientID | Name | Specialization
+
+SELECT t.PatientID, p.Name, d.Specialization 
+FROM Treatments t  
+JOIN patients p ON t.patientid = p.patientid
+JOIN doctors d ON t.doctorid = d.doctorid  
+WHERE t.PatientID IN (  
+    SELECT patientid  
+    FROM Treatments t2  
+    JOIN Doctors d2 ON t2.doctorid = d2.doctorid 
+    GROUP BY t2.patientid 
+    HAVING COUNT(DISTINCT d2.doctorid) > 1  
+)  
+ORDER BY t.PatientID , d.specialization ;
+
