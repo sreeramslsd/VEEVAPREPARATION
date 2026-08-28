e3323a11ff390e75b9befb8b8a0e754a124b3d53
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 10:41:13 +0530

    Add Task-4 Get All Patients (Unrated)

---
+++ codechef/Unrated/ASQLPR105 - Task-4 Get All Patients.cpp
@@ -0,0 +1,8 @@
+-- Write the query to Get All Patients Including Those Without Appointments (Use the concept of JOINs)
+
+-- The output should contain the following fields: 
+-- | Name | AppointmentID | Status |
+
+
+
+select p.name , a.appointmentid, a.status from patients p full outer join appointments a on a.patientid = p.patientid  ; 
