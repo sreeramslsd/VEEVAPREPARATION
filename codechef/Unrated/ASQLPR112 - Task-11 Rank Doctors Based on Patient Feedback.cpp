157e746aaaa7ebb3e36235214fc65f52732caef5
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 12:51:27 +0530

    Add Task-11 Rank Doctors Based on Patient Feedback (Unrated)

---
+++ codechef/Unrated/ASQLPR112 - Task-11 Rank Doctors Based on Patient Feedback.cpp
@@ -0,0 +1,14 @@
+-- Write the query to rank doctors based on patient feedback
+-- (Use the concept of Aggregations & Analytical Functions)
+
+-- The output should contain the following fields: 
+-- | DoctorID  | Name | Rank
+
+
+
+
+SELECT d.DoctorID, d.Name, 
+       RANK() OVER (ORDER BY AVG(t.FeedbackScore) DESC) AS Rank
+FROM Doctors d
+LEFT JOIN Treatments t ON d.DoctorID = t.DoctorID
+GROUP BY d.DoctorID, d.Name;
