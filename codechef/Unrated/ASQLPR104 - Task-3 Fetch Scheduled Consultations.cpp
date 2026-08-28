a6a2f6edb1ac92a15ae8038305f9c8aa02e6e562
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 10:28:17 +0530

    Add Task-3 Fetch Scheduled Consultations (Unrated)

---
+++ codechef/Unrated/ASQLPR104 - Task-3 Fetch Scheduled Consultations.cpp
@@ -0,0 +1,10 @@
+-- Write the query to fetch all Scheduled Consultations (Use the concept of JOINs)
+
+-- The output should contain the following fields: 
+-- | AppointmentID | Patient | Doctor | AppointmentDate |
+
+
+ 
+SELECT a.APPOINTMENTID ,  p.NAME AS PATIENT , d.NAME AS DOCTOR ,  a.APPOINTMENTDATE from appointments a join patients p on a.patientid = p.patientid 
+
+join doctors d on a.doctorid=d.doctorid limit 1;
