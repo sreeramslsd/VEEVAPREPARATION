cbb3a76ff8e0b839c7a3f220060ea565ff9104db
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 10:49:02 +0530

    Add Task-6 Patients Who Have Not Paid The Bill (Unrated)

---
+++ codechef/Unrated/ASQLPR107 - Task-6 Patients Who Have Not Paid The Bill.cpp
@@ -0,0 +1,12 @@
+-- Write the query to get the list of all patients who have not paid the bill. 
+-- (Use the concept of Subqueries & Set Operations)
+
+-- The output should contain the following fields: 
+-- |PatientID | Name |
+
+
+
+
+select p.patientid , p.name from patients p join Billing b on p.patientid = b.patientid where Status = 'Unpaid';
+
+
