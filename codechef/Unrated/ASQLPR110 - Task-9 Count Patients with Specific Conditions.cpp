3d3799933986b85247205c68720dc55ab18486ba
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 12:33:14 +0530

    Add Task-9 Count Patients with Specific Conditions (Unrated)

---
+++ codechef/Unrated/ASQLPR110 - Task-9 Count Patients with Specific Conditions.cpp
@@ -0,0 +1,11 @@
+-- Write the query to count the patients with specific conditions.
+-- (Use the concept of Aggregations & Analytical Functions)
+
+-- The output should contain the following fields: 
+-- |Diagnosis  | PatientCount
+
+
+
+
+
+SELECT DIAGNOSIS , COUNT(PATIENTID) AS PATIENTCOUNT FROM TREATMENTS GROUP BY DIAGNOSIS;
