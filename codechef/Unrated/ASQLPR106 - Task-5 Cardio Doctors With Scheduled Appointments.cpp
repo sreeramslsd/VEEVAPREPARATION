44b3db751f9ec724b00a452df0a40a2c6e8ad260
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 10:45:50 +0530

    Add Task-5 Cardio Doctors With Scheduled Appointments (Unrated)

---
+++ codechef/Unrated/ASQLPR106 - Task-5 Cardio Doctors With Scheduled Appointments.cpp
@@ -0,0 +1,10 @@
+-- Write the query to Get all Doctors from Cardiology Specialization With Scheduled Appointments 
+-- (Use the concept of JOINs)
+
+-- The output should contain the following fields: 
+-- | Name |
+
+
+
+
+select name from doctors d join appointments a on d.doctorid = a.doctorid where Status = 'Scheduled';
