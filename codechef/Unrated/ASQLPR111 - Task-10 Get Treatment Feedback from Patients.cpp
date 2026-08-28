ff39432e80710eb001ee8a34c7e3d871785a2ad8
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-28 12:44:22 +0530

    Add Task-10 Get Treatment Feedback from Patients (Unrated)

---
+++ codechef/Unrated/ASQLPR111 - Task-10 Get Treatment Feedback from Patients.cpp
@@ -0,0 +1,23 @@
+-- Write all the queries here & SUBMIT them all at once.
+-- Step 1: Write the query to alter the Treatments Table to include a column called 'FeedbackScore'
+
+
+-- Step 2: Write the queries to update the Treatments Table by inserting the new FeedbackScore values
+
+
+-- Step 3: Write the query to fetch and display all the records from the Treatments Table
+
+
+
+
+
+
+alter table Treatments add column FeedbackScore int check (FeedbackScore between 1 and 5);
+
+update Treatments set FeedBackScore = 5 where TreatmentID = 1;
+update Treatments set FeedBackScore = 4 where TreatmentID = 2;
+update Treatments set FeedBackScore = 3 where TreatmentID = 3;
+
+
+
+SELECT * FROM TREATMENTS;
