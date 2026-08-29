82fbd010a2d1bddfedce66a36db0c1ed41a985c8
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-29 15:08:13 +0530

    Add Task-3 Get order details with included products (Unrated)

---
+++ codechef/Unrated/ASQLPR205 - Task-3 Get order details with included products.cpp
@@ -0,0 +1,15 @@
+-- Write the query to retrieve order information along with the products included in each order.
+-- Headers expected in output: | OrderID | OrderDate  | FirstName | LastName |  ProductName   | Quantity | UnitPrice |
+
+
+
+
+
+select o.orderid, o.orderdate , c.firstname, c.lastname , p.productname , oo.quantity, oo.unitprice from Orders o join Customers c on o.customerid = c.customerid 
+
+
+join products p on p.productid = oo.productid 
+
+join orderdetails oo on o.orderid = oo.orderid
+
+;
