fc659e3c63768c943e4ebe16bcbfb50dbcd672dc
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-29 15:30:07 +0530

    Add Task-4 Customers who have not placed any orders (Unrated)

---
+++ codechef/Unrated/ASQLPR206 - Task-4 Customers who have not placed any orders.cpp
@@ -0,0 +1,10 @@
+SELECT 
+    C.CustomerID, 
+    C.FirstName, 
+    C.LastName 
+FROM 
+    Customers AS C 
+LEFT JOIN 
+    Orders AS O ON C.CustomerID = O.CustomerID 
+WHERE 
+    O.OrderID IS NULL;
