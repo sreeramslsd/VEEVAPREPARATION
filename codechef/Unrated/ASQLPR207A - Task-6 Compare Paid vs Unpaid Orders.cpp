8f055c2f39b20f631835d8045ecbf494f348a331
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-31 10:14:11 +0530

    Add Task-6 Compare Paid vs Unpaid Orders (Unrated)

---
+++ codechef/Unrated/ASQLPR207A - Task-6 Compare Paid vs Unpaid Orders.cpp
@@ -0,0 +1,7 @@
+-- Write the query to compare orders that have associated payments with orders that do not.
+-- Headers expected in output: | OrderID | OrderDate  | PaymentID | PaymentDate | Amount |
+
+
+select o.orderid, o.orderdate, p.paymentid , p.paymentdate, p.amount from orders o join payments p on o.orderid = p.orderid ; 
+
+
