33980e7591cc6bd6696138accbd4014c69f8c45b
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-31 14:54:05 +0530

    Add Task-9 Combine and order customers by region (Unrated)

---
+++ codechef/Unrated/ASQLPR210 - Task-9 Combine and order customers by region.cpp
@@ -0,0 +1,13 @@
+SELECT 
+    CustomerID, 
+    FirstName, 
+    LastName, 
+    City, 
+    CASE 
+        WHEN Region IS NULL THEN 'Not Specified' 
+        ELSE Region 
+    END AS Region, 
+    Country 
+FROM Customers 
+WHERE Country IN ('USA', 'UK', 'France') 
+ORDER BY Country, LastName ASC;
