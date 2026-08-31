1dcba0ba5a3a057f59d02c1f047d4135e263a1f9
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-31 14:46:57 +0530

    Add Task-8 Find each customers highest total order (Unrated)

---
+++ codechef/Unrated/ASQLPR209 - Task-8 Find each customers highest total order.cpp
@@ -0,0 +1,29 @@
+SELECT
+    O.OrderID,
+    O.CustomerID,
+    O.OrderDate,
+    OrderTotals.TotalOrderAmount
+FROM Orders AS O
+INNER JOIN 
+(
+    SELECT
+        OrderID,
+        SUM(Quantity * UnitPrice) AS TotalOrderAmount
+    FROM OrderDetails
+    GROUP BY OrderID
+) AS OrderTotals ON O.OrderID = OrderTotals.OrderID
+WHERE O.OrderID = (
+    SELECT O2.OrderID
+    FROM Orders AS O2
+    INNER JOIN 
+    (
+        SELECT
+            OrderID,
+            SUM(Quantity * UnitPrice) AS TotalOrderAmount
+        FROM OrderDetails
+        GROUP BY OrderID
+    ) AS OrderTotals2 ON O2.OrderID = OrderTotals2.OrderID
+    WHERE O2.CustomerID = O.CustomerID
+    ORDER BY OrderTotals2.TotalOrderAmount DESC
+    LIMIT 1
+);
