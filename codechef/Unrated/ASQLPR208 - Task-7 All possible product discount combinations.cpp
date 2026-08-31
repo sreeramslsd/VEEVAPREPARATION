3cead9ce2c04c860eb3c4ecf1761a062225967a3
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-31 10:29:21 +0530

    Add Task-7 All possible product discount combinations (Unrated)

---
+++ codechef/Unrated/ASQLPR208 - Task-7 All possible product discount combinations.cpp
@@ -0,0 +1,7 @@
+SELECT 
+    P.ProductName, 
+    D.DiscountName, 
+    D.DiscountPercentage, 
+    P.Price * (1 - D.DiscountPercentage) AS DiscountedPrice 
+FROM Products AS P
+CROSS JOIN Discounts AS D;
