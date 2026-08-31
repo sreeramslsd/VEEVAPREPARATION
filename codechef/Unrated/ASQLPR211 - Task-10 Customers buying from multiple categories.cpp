f46f3c4fc958316e06c085b2626d6612dc901489
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-08-31 15:20:12 +0530

    Add Task-10 Customers buying from multiple categories (Unrated)

---
+++ codechef/Unrated/ASQLPR211 - Task-10 Customers buying from multiple categories.cpp
@@ -0,0 +1,13 @@
+SELECT C.CustomerID
+FROM Customers C
+JOIN Orders O ON C.CustomerID = O.CustomerID
+JOIN OrderDetails OD ON O.OrderID = OD.OrderID
+JOIN Products P ON OD.ProductID = P.ProductID
+WHERE P.CategoryID = 1 
+INTERSECT
+SELECT C.CustomerID
+FROM Customers C
+JOIN Orders O ON C.CustomerID = O.CustomerID
+JOIN OrderDetails OD ON O.OrderID = OD.OrderID
+JOIN Products P ON OD.ProductID = P.ProductID
+WHERE P.CategoryID = 2;
