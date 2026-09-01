41b69a71973731db463cff385ade5466d3471448
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-01 16:21:06 +0530

    Add Task-18 Categorize products into four price tiers (Unrated)

---
+++ codechef/Unrated/ASQLPR219 - Task-18 Categorize products into four price tiers.cpp
@@ -0,0 +1,12 @@
+-- Write the query to divide products into four price groups (quartiles) within each category.
+-- Create a temporary column called `PriceQuartile` 
+-- Use the `NTILE` function
+-- Headers expected in output: | CategoryID | ProductName | Price | PriceQuartile |
+
+SELECT
+    CategoryID,
+    ProductName,
+    Price,
+    NTILE(4) OVER (PARTITION BY CategoryID ORDER BY Price) AS PriceQuartile
+FROM
+    Products;
