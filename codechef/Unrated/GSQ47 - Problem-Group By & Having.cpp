91dea83c8fea7c39967d7e084e67c1fc409263bf
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-15 11:37:57 +0530

    Add Problem-Group By & Having (Unrated)

--- codechef/Unrated/GSQ47 - Problem-Group By & Having.cpp
+++ codechef/Unrated/GSQ47 - Problem-Group By & Having.cpp
@@ -1,10 +1,8 @@
 /* Write a query to output the following
 - Department and average payout on a single line
 - Where total payout of the department is more than 40. */
- 
- 
 
 
 
 
-SELECT department, AVG(PAYOUT) AS AVG_PAYOUT FROM EMPLOYEE GROUP BY DEPARTMENT HAVING SUM(PAYOUT) > 40;
+select department, avg(payout) as avg_payout from employee  group by department having sum(payout) > 40;
