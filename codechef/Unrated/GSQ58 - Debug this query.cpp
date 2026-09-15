026cf4d552144d8ba9323d5a119944600bd94afc
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-15 11:28:37 +0530

    Add Debug this query (Unrated)

--- codechef/Unrated/GSQ58 - Debug this query.cpp
+++ codechef/Unrated/GSQ58 - Debug this query.cpp
@@ -1,7 +1,9 @@
 /* **Debug this query** to output the minimum and maximum value of payout, round to 2 decimal places. */
 
 
-select min(round(Payout,2)) as 'min_pay',
-    max(round(Payout,2)) as 'max_pay'
-from employee limit 1;
 
+
+
+
+select round(min(payout),2) as min_pay , round(max(payout),2 )
+as max_pay from employee
