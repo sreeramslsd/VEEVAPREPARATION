b2a98476190c3a55c25b10eb6d72443d268bbf0c
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-15 11:35:11 +0530

    Add Having (Unrated)

--- codechef/Unrated/GSQ46 - Having.cpp
+++ codechef/Unrated/GSQ46 - Having.cpp
@@ -4,5 +4,4 @@ The output table should have the name of the department and their respective ave
 
 
 
-
-SELECT department , AVG(Payout) AS AVG_PAYOUT FROM EMPLOYEE GROUP BY DEPARTMENT HAVING COUNT (*) > 3;
+select department, avg(Payout) as avg_payout from employee group by department having count(*) > 3;
