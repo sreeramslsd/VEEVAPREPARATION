2e6d7cbcf603d3b1208a25d4a0b51c8c7f43d1be
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-15 11:22:23 +0530

    Add Project - Debug this query (Unrated)

--- codechef/Unrated/GSQ36B - Project - Debug this query.cpp
+++ codechef/Unrated/GSQ36B - Project - Debug this query.cpp
@@ -1,5 +1,4 @@
-select customer_name
-from Orders
-where (address_pincode = 122001 OR address_pincode = 122002)
-and order_time < 12.3
-and apartment_floor >= 3;
+
+
+
+select customer_name from orders where (address_pincode  = 122001 or address_pincode = 122002 ) and order_time<12.30 and apartment_floor >= 3;
