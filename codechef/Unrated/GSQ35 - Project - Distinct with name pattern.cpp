886912fb2d5d9bd383d49de0a7e6bf4e12ebe027
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-15 11:18:19 +0530

    Add Project - Distinct with name pattern (Unrated)

--- codechef/Unrated/GSQ35 - Project - Distinct with name pattern.cpp
+++ codechef/Unrated/GSQ35 - Project - Distinct with name pattern.cpp
@@ -1,4 +1,5 @@
 /* Write a query to output the userids of all distinct users whose name end with the letter 'a' */
 
 
-select distinct customer_id from Orders where customer_name like '%a';
+
+select distinct(customer_id) from orders where customer_name like '%a';
