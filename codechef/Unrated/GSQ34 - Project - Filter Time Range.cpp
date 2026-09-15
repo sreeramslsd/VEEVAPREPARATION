16273ebfec8801960211d1377f5bb4cd5c127ccd
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-15 11:17:25 +0530

    Add Project - Filter Time Range (Unrated)

--- codechef/Unrated/GSQ34 - Project - Filter Time Range.cpp
+++ codechef/Unrated/GSQ34 - Project - Filter Time Range.cpp
@@ -1,3 +1,6 @@
 /* Write a query to output the all the data for orders received between 12 pm and 1 pm. */
 
-select * from Orders where order_time between 12.00 and 13.00;
+
+
+
+select * from orders where  order_time between 12 and 13;
