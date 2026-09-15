39b5418056ec9ef38b8495167f480da6ab638f88
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-15 11:16:09 +0530

    Add Project - Distinct values (Unrated)

--- codechef/Unrated/GSQ33 - Project - Distinct values.cpp
+++ codechef/Unrated/GSQ33 - Project - Distinct values.cpp
@@ -1,5 +1,4 @@
 /* Write a query to output the distinct pin-codes from where orders have been received. */
 
 
-
-select distinct address_pincode from orders;
+select distinct(address_pincode) from orders;
