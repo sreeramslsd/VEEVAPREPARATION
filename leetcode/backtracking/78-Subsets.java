fcb5a33304699bc7563e2afde52a20a039a163d6
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-24 16:08:57 +0530

    Add Subsets (backtracking)

--- leetcode/backtracking/78-Subsets.java
+++ leetcode/backtracking/78-Subsets.java
@@ -1,24 +1,20 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-
+        
         List<List<Integer>> al = new ArrayList<>();
+    al.add(new ArrayList<>());
+    for(Integer num : nums)
+    {
+          int size = al.size();
+          for(int i=0;i<size;i++)
+          {
+             ArrayList<Integer> sd = new ArrayList<>(al.get(i));        
+                sd.add(num);
+                al.add(sd);
+          } 
+    }
+return al;
 
-        al.add(new ArrayList<>());
 
-        for(int num : nums) {
-
-            int size = al.size();
-
-            for(int i = 0; i < size; i++) {
-
-                List<Integer> temp = new ArrayList<>(al.get(i));
-
-                temp.add(num);
-
-                al.add(temp);
-            }
-        }
-
-        return al;
     }
 }
