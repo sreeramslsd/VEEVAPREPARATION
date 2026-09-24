e9a45e95a741282e6c75f79e735171e1a57845e2
DANGETI DURGA MURALI SRIRAM <sreeramslsd@gmail.com>
2026-09-24 17:02:47 +0530

    Add Remove-Duplicates-from-Sorted-List-II (two-pointers)

---
+++ leetcode/two-pointers/82-Remove-Duplicates-from-Sorted-List-II.java
@@ -0,0 +1,35 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        if(head == null || head.next == null)
+            return head;
+
+        ListNode temp = head;
+        ListNode dummy = null;
+
+        while(temp != null) {
+
+            if(temp.next != null && temp.val == temp.next.val) {
+
+                int value = temp.val;
+
+                while(temp != null && temp.val == value) {
+                    temp = temp.next;
+                }
+
+                if(dummy == null)
+                    head = temp;
+                else
+                    dummy.next = temp;
+
+            }
+            else {
+
+                dummy = temp;
+                temp = temp.next;
+            }
+        }
+
+        return head;
+    }
+}
